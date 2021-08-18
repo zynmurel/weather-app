b156241f0f121f1060932e9fb750ddd6cb6126f2
maui <comingues99@gmail.com>
2021-08-18 22:30:27 +0800

    add: search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Logo from '@components/atoms/Logo'
+import FreeSolo from './Components/input'
+
 
 const Home: React.FC = () => {
-  return <Logo />
+  return (
+      <div style={{"margin": "auto", "background-color": "#cfe8fc", maxWidth: 1200, "margin-top": 50, padding: 20}}>
+           <FreeSolo/>
+      </div>
+  )
 }
 export default Home
