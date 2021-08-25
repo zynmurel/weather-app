2594803129ab587e944de8326c718895a0d31ed3
Sean Maruel <comingues99@gmail.com>
2021-08-25 22:08:36 +0800

    calling places from weather-api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <FreeSolo />
-      <CardGrid></CardGrid>
+      <CardGrid />
     </div>
   )
 }
--- src/api/weatherService.ts
+++ src/api/weatherService.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const getSuggestedPlaces = async (query: string) =>{
-  const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`)
-
-    return response
+export const getSuggestedPlaces = async (query: string) => {
+  return await axios.get(
+    `https://api.weatherapi.com/v1/search.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${query}`
+  )
 }
 // https://api.weatherapi.com/v1/current.json?key=ac6a35ef289843838c3112303212308&q=BIla&aqi=no
